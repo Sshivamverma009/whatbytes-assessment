949bd2d8288d0c7435f49a2ac0dd7ec9748f274a
Sshivamverma009 <shivamverma.sv777@gmail.com>
2025-05-21 21:54:37 +0530

    change in cart route

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,31 +1,7 @@
 "use client";
 
-import { useSelector } from "react-redux";
-import { RootState } from "@/lib/store";
-import Card from "@/components/Card";
-import { products } from "@/components/ProductGrid";
-
 export default function Cart() {
-  const cart = useSelector((state: RootState) => state.store.cart);
-
-  const cartProducts = products.filter(
-    (product) => typeof product.id === "string" && cart.hasOwnProperty(product.id)
-  );
-
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Cart</h2>
-      {cartProducts.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {cartProducts.map((product) => (
-            <li key={product.id}>
-              <Card product={product} />
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+  return(
+    <div>Har Har Mahadev</div>
+  )
 }
